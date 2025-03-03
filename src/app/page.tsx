b93d8c4fcc952d2a1e56708f9458533570d98123
Diyorbekabdumannonov <94838816+Diyorbekabdumannonov@users.noreply.github.com>
2025-03-03 18:53:25 +0500

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import WhyChooseUs from '@/components/sections/WhyChooseUs';
 
 export default function Home() {
   return (
-    <>
+    <> 
       <div
         className="md:bg-hero md:bg-cover md:bg-center md:bg-no-repeat">
         <Navbar />
