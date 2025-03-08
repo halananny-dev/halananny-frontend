bdac9be02a7629ce67e1b15e878c9ad0104275b1
Diyorbekabdumannonov <94838816+Diyorbekabdumannonov@users.noreply.github.com>
2025-03-08 10:41:02 +0500

    change password

--- src/components/dashboard/ChangePassword.tsx
+++ src/components/dashboard/ChangePassword.tsx
@@ -1,67 +1,66 @@
 "use client"
 
+import { useAppContext } from "@/i18/AppContext"
 import { useI18n } from "@/i18/i18Context"
-import { useRef, useState } from "react"
+import { supabase } from "@/lib/supabase"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
 import { toast } from "react-toastify"
 import Btn from "../sections/Button"
+import Loader from "../sections/Loader"
 import { Input } from "../ui/input"
 
 export default function ChangePassword() {
-	const [disabled, setDisabled] = useState(true)
-	const formRef = useRef<HTMLFormElement>(null)
 	const { t } = useI18n()
+	const { user } = useAppContext()
+	const [loading, setLoading] = useState(false)
+	const {
+		handleSubmit,
+		register,
+		watch,
+		formState: { isValid },
+	} = useForm({ mode: "onChange" })
 
-	const handleChange = () => {
-		const form = new FormData(formRef.current!)
+	const newPassword = watch("new_password")
+	const confirmNewPassword = watch("confirm_new_password")
 
-		const current_password = form.get("current_password")?.toString().trim()
-		const npassword = form.get("npassword")?.toString().trim()
-		const cnpassword = form.get("cnpassword")?.toString().trim()
+	const onSubmit = async (data) => {
+		setLoading(true)
+		const { error: signInError } = await supabase.auth.signInWithPassword({
+			email: user.email,
+			password: data.current_password,
+		})
 
-		setDisabled(!current_password || !npassword || !cnpassword)
-	}
-
-	const handleSubmit = (event: React.FormEvent) => {
-		event.preventDefault()
-
-		const form = new FormData(formRef.current!)
-
-		const npassword = form.get("npassword")?.toString().trim()
-		const cnpassword = form.get("cnpassword")?.toString().trim()
-
-		if (npassword !== cnpassword) {
-			toast.error(t.Login["Passwords don't match!"])
+		if (signInError) {
+			toast.error(t.Login["Incorrect current password!"])
 		}
 		else {
+			await supabase.auth.updateUser({ password: data.new_password })
 			toast.success(t.Login["Updated successfully!"])
 		}
+
+		setLoading(false)
 	}
 
 	return (
-		<form ref={formRef} onChange={handleChange} onSubmit={handleSubmit}>
-			<div className="flex flex-col gap-4 w-full text-gray-900 font-bold">
-				<label htmlFor="current_password">{t.Login.current_password}</label>
-				<Input name="current_password" type="password" id="current_password" />
-			</div>
+		<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+			<label className="text-gray-900 font-bold">{t.Login.current_password}</label>
+			<Input {...register("current_password", { required: true })} type="password" />
 
-			<div className="flex mt-8 flex-col gap-4 w-full text-gray-900 font-bold">
-				<label htmlFor="npassword">{t.Login.new_password}</label>
-				<Input name="npassword" type="password" id="npassword" />
-			</div>
+			<label className="text-gray-900 font-bold">{t.Login.new_password}</label>
+			<Input {...register("new_password", { required: true })} type="password" />
 
-			<div className="flex mt-4 flex-col gap-4 w-full text-gray-900 font-bold">
-				<label htmlFor="cnpassword">{t.Login['Confirm New Password']}</label>
-				<Input name="cnpassword" type="password" id="cnpassword" />
-			</div>
+			<label className="text-gray-900 font-bold">{t.Login['Confirm New Password']}</label>
+			<Input {...register("confirm_new_password", {
+				required: true,
+				validate: value => value === newPassword || t.Login["Passwords don't match!"]
+			})} type="password" />
 
-			<Btn
-				type="submit"
-				disabled={disabled}
-				className="mt-8"
-				size="lg"
-				variant="primary"
-			>
+			<Btn type="submit"
+				disabled={!isValid || newPassword !== confirmNewPassword || loading}
+				className="mt-8" size="lg" variant="primary">
 				{t.Login['Change Password']}
+				{loading && <Loader className="ltr:ml-2 rtl:mr-2" />}
 			</Btn>
 		</form>
 	)
